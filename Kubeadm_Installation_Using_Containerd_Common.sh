f51e4a7e37ba1807e84bcb986d0d558a9ebb5eff
Gaurav Mathur <ecegauravmathur90@gmail.com>
2024-11-13 18:18:49 +0530

    Changed sleep time from 5 to 2

--- Kubeadm_Installation_Using_Containerd_Common.sh
+++ Kubeadm_Installation_Using_Containerd_Common.sh
@@ -5,7 +5,7 @@
 # 1. Disable Swap: Required for Kubernetes to function correctly.
 echo "Disabling swap..."
 sudo swapoff -a
-sleep 5
+sleep 2
 
 # 2. Load Necessary Kernel Modules: Required for Kubernetes networking.
 echo "Loading necessary kernel modules for Kubernetes networking..."
@@ -16,7 +16,7 @@ EOF
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
-sleep 5
+sleep 2
 
 # 3. Set Sysctl Parameters: Helps with networking.
 echo "Setting sysctl parameters for networking..."
@@ -29,57 +29,57 @@ EOF
 sudo sysctl --system
 lsmod | grep br_netfilter
 lsmod | grep overlay
-sleep 5
+sleep 2
 
 # 4. Install Containerd:
 echo "Installing containerd..."
 sudo apt-get update
-sleep 5
+sleep 2
 
 sudo apt-get install -y ca-certificates curl
-sleep 5
+sleep 2
 
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-sleep 5
+sleep 2
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sleep 5
+sleep 2
 
 sudo apt-get install -y containerd.io
-sleep 5
+sleep 2
 
 containerd config default | sed -e 's/SystemdCgroup = false/SystemdCgroup = true/' -e 's/sandbox_image = "registry.k8s.io\/pause:3.6"/sandbox_image = "registry.k8s.io\/pause:3.9"/' | sudo tee /etc/containerd/config.toml
 
 sudo systemctl restart containerd
-sleep 5
+sleep 2
 
-sudo systemctl status containerd
-sleep 5
+sudo systemctl status containerd &
+sleep 2
 
 # 5. Install Kubernetes Components:
 echo "Installing Kubernetes components (kubelet, kubeadm, kubectl)..."
 sudo apt-get update
-sleep 5
+sleep 2
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-sleep 5
+sleep 2
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sleep 5
+sleep 2
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
-sleep 5
+sleep 2
 
 sudo apt-get install -y kubelet kubeadm kubectl
-sleep 5
+sleep 2
 
 sudo apt-mark hold kubelet kubeadm kubectl
-sleep 5
+sleep 2
 
 echo "Kubernetes setup completed."
