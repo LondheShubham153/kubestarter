8a2ef1aecd198d49e40fcfc72a6ccda90d444868
Shubham Londhe <shubhamnath5@gmail.com>
2024-11-19 21:28:10 +0530

    Create install.sh

---
+++ kind-cluster/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# For AMD64 / x86_64
+
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo cp ./kind /usr/local/bin/kind
+
+
+VERSION="v1.30.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
+# Download and install kubectl
+curl -LO "$URL"
+chmod +x kubectl
+sudo mv kubectl $INSTALL_DIR/
+kubectl version --client
+
+# Clean up
+rm -f kubectl
+rm -rf kind
+
+echo "kind & kubectl installation complete."
