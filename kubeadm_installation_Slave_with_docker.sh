4dbcaa31e53d6d3ca33487561205a8336e35f7b2
Gaurav Mathur <ecegauravmathur90@gmail.com>
2024-11-20 11:43:29 +0530

    Create kubeadm_installation_Slave_with_docker.sh

---
+++ kubeadm_installation_Slave_with_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo su
+apt update -y
+apt install docker.io -y
+
+systemctl start docker
+systemctl enable docker
+
+curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
+
+apt update -y
+apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+
+------------------------------------------- Worker Node ------------------------------------------------ 
+sudo su
+kubeadm reset pre-flight checks
+# -----> Paste the Join command on worker node and append `--v=5` at end
+
+#To verify cluster connection  
