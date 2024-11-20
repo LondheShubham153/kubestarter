271de56354f6b7a42e7e97e7d7f128439169bb0c
Gaurav Mathur <ecegauravmathur90@gmail.com>
2024-11-20 12:10:02 +0530

    Update Kubeadm_Installation_Slave_Using_Containerd.sh

--- Kubeadm_Installation_Slave_Using_Containerd.sh
+++ Kubeadm_Installation_Slave_Using_Containerd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Execute on ALL of your Worker Nodes
+#Execute on ALL of your Worker Nodes
 
-# 1. Perform pre-flight checks:
+#1. Perform pre-flight checks:
 ```bash
 sudo kubeadm reset pre-flight checks
 ```
 
-# 2. Paste the join command you got from the master node and append --v=5 at the end:
+#2. Paste the join command you got from the master node and append --v=5 at the end:
 
 #sudo kubeadm join <control-plane-ip>:6443 --token <token> --discovery-token-ca-cert-hash sha256:<hash> --cri-socket "unix:///run/containerd/containerd.sock" --v=5
 
