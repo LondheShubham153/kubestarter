ce202b81eb37b5462014d2680eecfe4902a9f7a9
Gaurav Mathur <ecegauravmathur90@gmail.com>
2024-11-21 11:32:00 +0530

    Update Kubeadm_Installation_Slave_Using_Containerd.sh

--- Kubeadm_Installation_Scripts_and_Documentation/Kubeadm_Installation_Slave_Using_Containerd.sh
+++ Kubeadm_Installation_Scripts_and_Documentation/Kubeadm_Installation_Slave_Using_Containerd.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#Execute on ALL of your Worker Nodes
+# Execute on ALL of your Worker Nodes
 
-#1. Perform pre-flight checks and reset the node:
-```bash
+# 1. Perform pre-flight checks and reset the node:
 sudo kubeadm reset -f
-```
 
 # 2. Paste the join command you got from the master node and append --v=5 at the end:
 # Example:
